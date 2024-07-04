d6aebd270195b685f99bd49666c15521ef2532ca
Ankur Kumar <sandyankurkumar@gmail.com>
2024-07-04 18:37:01 +0530

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
-  userState: userReducer, /
+  userState: userReducer, 
 });
 
 export default rootReducer;
